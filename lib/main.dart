c4225e77dff161eceb4041ffde15a32827ca5865
Aaron Kelton <6170077+aaronkelton@users.noreply.github.com>
2023-06-17 14:41:33 -0500

    improved Cubit implementation replacing StreamBuilder with BlocBuilder
    * stream becomes bloc (to be explicit; see docs)
    * no need for initialData anymore; the BlocBuilder just knows more about it's specified bloc
    * rather than snapshot.data; we just pass in the state; yes this is a better API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,30 +14,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Counter',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Counter with Cubit/Stream'),
+      home: const MyHomePage(title: 'Counter with Cubit/BlocBuilder'),
     );
   }
 }
 
 class CounterCubit extends Cubit<int> {
-  // by virtue of having this <int> notation, and
-  // the super param of the same type (post-colon initializer list):
-  // we magically have the *_state* variable
-  CounterCubit(int initialCount) : super(initialCount) {
-    emitInitialState(); // TODO: make this work first
-  }
-  void emitInitialState() => emit(state); // TODO: make this work first
+  CounterCubit(int initialCount) : super(initialCount);
 
-  // CounterCubit(int initialCount) : super(0); // super overrides our input
-  // CounterCubit() : super(0); // constructor takes no params; hardcoding initial state
-
-  void increment() => emit(state + 1); // state is just a getter for _state
-
+  void increment() => emit(state + 1);
 }
 
-/// We really want to use a StatefulWidget for performance reasons
-/// We can use a Stateless and it'll work, but it's not ideal.
-/// Read the docs on Stateless for a better idea.
 class MyHomePage extends StatefulWidget {
   final String title;
 
@@ -50,33 +38,22 @@ class _MyHomePageState extends State<MyHomePage> {
   final CounterCubit _counter = CounterCubit(0);
 
   @override
-  void initState() {
-    _counter.emitInitialState(); // TODO: fix this to work per docs
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            StreamBuilder<int>(
-              stream: _counter.stream,
-              // initialData: _counter.state,
-              builder: (context, snapshot) {
+            const Text('Counter:'),
+            BlocBuilder(
+              bloc: _counter,
+              builder: (context, state) {
                 return Text(
-                  '${snapshot.data}',
+                  '$state',
                   style: Theme.of(context).textTheme.headline4,
                 );
-              }
+              },
             ),
           ],
         ),
